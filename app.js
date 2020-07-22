195d34388c56232094a310a4bb048a029b811e08
ChenXv <1040255945>
2020-07-22 16:46:10 +0800

    722

--- app.js
+++ app.js
@@ -5,8 +5,31 @@ new Vue({
         website: "https://www.baidu.com",
         year: 2020,
         x: 0,
-        y: 0
+        y: 0,
+        a: 10,
+        b: 0,
+        c: 0,
+        age: 20,
+        available: false,
+        nearby: false,
+        characters: ['Mario', 'Luigi', 'Yoshi', 'Bowser'],
+        ninjas: [{
+                name: 'Ryu',
+                age: 25
+            },
+            {
+                name: 'Yoshi',
+                age: 35
+            },
+            {
+                name: 'Ken',
+                age: 55
+            }
+        ],
+        health: 100,
+        ended: false
     },
+
     methods: {
         greet: function (e) {
             return "Good " + e;
@@ -18,6 +41,39 @@ new Vue({
             //offsetX/Y获取到是触发点相对被触发dom的左上角距离
             this.x = e.offsetX;
             this.y = e.offsetY;
+        },
+        showVal: function () {
+            alert(this.$refs["showVal"].value)
+        },
+        punch: function () {
+            this.health -= 10;
+            if (this.health <= 0) {
+                this.ended = true;
+            }
+        },
+        restart: function () {
+            this.health = 100;
+            this.ended = false;
+        }
+    },
+    watch: {
+        a(val, oldVal) {
+            alert(val + " " + oldVal);
+        }
+    },
+    computed: {
+        addToB() {
+            // console.log(this)
+            return this.b + this.age;
+        },
+        addToC() {
+            return this.c + this.age;
+        },
+        compClasses: function () {
+            return {
+                available: this.available,
+                nearby: this.nearby,
+            }
         }
     }
 })
